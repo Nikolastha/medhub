34297ddff3175db5ecfae469190440412f729f0d
rajat.shrestha.pop <rajat.shrestha@eydean.com>
2020-07-10 08:15:16 +0545

    Removed password 2 and major refactoring

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
+"""
+backend simple server for the pharmacy app med-hub
+"""
+
+import os
 from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import render_template
 from flask_cors import CORS
-import bcrypt
+from bcrypt import hashpw
+from bcrypt import gensalt
 from dotenv import load_dotenv
-import os
-
 from pymongo import MongoClient
 
 
 load_dotenv()
-
-client = MongoClient(os.getenv('MC'))
-#     "mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false")
-
-print(type(os.getenv('DEBUG')), str(os.getenv('DEBUG')))
-print(type(os.getenv('MC')), str(os.getenv('MC')))
 app = Flask(__name__)
 CORS(app)
 app.config['MONGO_DBNAME'] = 'mongo'
-
-
+client = MongoClient(os.getenv('MC'))
 mydb = client["pharmacy"]
 medicine = mydb["medicine"]
 users = mydb["users"]
@@ -29,18 +26,24 @@ users = mydb["users"]
 
 @app.route('/', methods=['GET'])
 def index():
+    """ Index page
+
+    :return: index page
+    :rtype: flask rendertemplate
+    """
     return render_template('index.html')
 
 
 @app.route('/register', methods=['POST'])
 def register():
+    """ Simple post request to register a new user using json"""
     return_json = {}
     if request.method == 'POST':
         post_data = request.json
         return_json["post_data"] = post_data
 
         pass1 = post_data["password_1"].encode('utf-8')
-        pass2 = post_data["password_2"].encode('utf-8')
+        # pass2 = post_data["password_2"].encode('utf-8')
         license_agreement = post_data["license_agreement"]
         _id = post_data["phone"]
         existing_user = users.find_one({'_id': _id})
@@ -52,20 +55,20 @@ def register():
 
         if existing_user is None:
             if len(pass1) >= 6:
-                if pass1 == pass2:
-                    if license_agreement:
-                        hashpass = bcrypt.hashpw(pass1, bcrypt.gensalt())
-                        users.insert({
-                            '_id': _id,
-                            'password': hashpass,
-                            'first': first,
-                            'last': last,
-                            'gender': gender,
-                            'dob': dob
-                        })
-                        return jsonify({"result": 'User added!'})
-                    return jsonify({"result": 'Error 5: Please agree the license agreement'})
-                return jsonify({"result": 'Error 4: passwords do not match'})
+                # if pass1 == pass2:
+                if license_agreement:
+                    hashpass = hashpw(pass1, gensalt())
+                    users.insert({
+                        '_id': _id,
+                        'password': hashpass,
+                        'first': first,
+                        'last': last,
+                        'gender': gender,
+                        'dob': dob
+                    })
+                    return jsonify({"result": 'User added!'})
+                return jsonify({"result": 'Error 5: Please agree the license agreement'})
+                # return jsonify({"result": 'Error 4: passwords do not match'})
             return jsonify({"result": 'Error 3: Set stronger password with atleast 6 characters'})
         return jsonify({"result": 'Error 2: That _id already exists!'})
     return jsonify({"result": "Error 1: post registration details"})
@@ -73,6 +76,8 @@ def register():
 
 @app.route('/login', methods=['POST'])
 def login():
+    """ simple post request to login using json """
+
     return_json = {}
     if request.method == 'POST':
         post_data = request.json
@@ -81,10 +86,8 @@ def login():
         password = post_data["password"]
         login_user = users.find_one({'_id': phone})
         if login_user and password != "":
-            x = password.encode('utf-8')
-            y = login_user['password']
-            z = login_user['password']
-            if bcrypt.hashpw(x, y) == z:
+            user_pass = login_user['password']
+            if hashpw(password.encode('utf-8'), user_pass) == user_pass:
                 return_json["success"] = True
                 return_json["message"] = "Authentication success!"
             else:
@@ -101,54 +104,42 @@ def login():
 
 @app.route('/meds', methods=['GET'])
 def get_all_medicine():
+    """ get all medicines """
     return jsonify({"result": list(medicine.find())})
 
 
 @app.route('/med', methods=['GET', 'POST'])
 def get_one_med():
+    "get a single medicine"
     common_name = request.args.get('common_name')
-    s = medicine.find_one({'common_name': common_name})
-    if s:
-        output = s
+    search_medicine = medicine.find_one({'common_name': common_name})
+    if search_medicine:
+        output = search_medicine
     else:
         output = "No such name Err1" + str(common_name)
     return jsonify({'result': output})
 
 
-@app.route('/addmed', methods=['POST'])
-def add_med():
-    med_id = request.args.get('med_id')
-    med = {
-        "_id": med_id,
-        "common_name": request.args.get('common_name'),
-        "technical_name": request.args.get('technical_name'),
-        "price": request.args.get('price'),
-        "schedule": request.args.get('schedule'),
-        "prescription": request.args.get('prescription'),
-        "description": request.args.get('description'),
-        "image_url": request.args.get('image_url')}
-
-    output = medicine.insert_one(med)
-    return jsonify({'result': str(output)})
-
-
 @app.route('/addmedicine')
 def addmedicine():
-   return render_template('medicine.html')
+    """ add a new medicine using a webform """
+    return render_template('medicine.html')
 
-@app.route('/addmedicine_post',methods = ['POST', 'GET'])
+
+@app.route('/addmedicine_post', methods=['POST', 'GET'])
 def addmedicine_post():
+    """ post requst which will add the posted json to medicine collection"""
     if request.method == 'POST':
         medicine_dict = {
-            'technical_name' : request.form['technical_name'],
-            'common_name' : request.form['common_name'],
-            'price' : request.form['price'],
-            'schedule' : request.form['schedule'],
-            'description' : request.form['description'],
-            'prescription' : request.form['prescription'],
-            'company' : request.form['company'],
-            '_id' : request.form['id'],
-            'image_url' : request.form['image_url']
+            'technical_name': request.form['technical_name'],
+            'common_name': request.form['common_name'],
+            'price': request.form['price'],
+            'schedule': request.form['schedule'],
+            'description': request.form['description'],
+            'prescription': request.form['prescription'],
+            'company': request.form['company'],
+            '_id': request.form['id'],
+            'image_url': request.form['image_url']
         }
         medicine.insert_one(medicine_dict)
         output = "Medicine added!"
@@ -159,4 +150,4 @@ def addmedicine_post():
 
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('DEBUG'))
+    app.run(debug=bool(os.getenv('DEBUG')))
